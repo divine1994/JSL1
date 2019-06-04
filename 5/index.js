56c233d7d7cd4434848d2571f87ad4d5c1474a4a
Roman <divine1994@yandex.ru>
2019-06-04 18:00:34 +0300

    3

--- 5/index.js
+++ 5/index.js
@@ -1,9 +1,29 @@
 
-let http =require('http')
+let http =require('http');
 
+function getApi(result) {
+//создаем функцию с аргументом result,куда будем передавать массив данных из запроса.
+    let shitprice = {date:'0',price:99999};
+    //парсим результат и получаем массив
+    let data = JSON.parse(result);
+    for (price of data.best_prices)
+        {
+            let date = price.depart_date.split('-');
+        if (+date[0] === 2019 && +date[1] === 9 || +date[1] === 10 || +date[1] === 11)
+            {
+            if (price.value < shitprice.price )
+                {
+                shitprice.price = price.value;
+                shitprice.date = price.depart_date;
+                }
+
+            }
+        }
+    console.log(("Дешевле всего лететь из Москвы в Ниццу " + shitprice.date + " Это будет стоить " + shitprice.price + "руб."));
+}
 
 //Когда лень получать массив данных с api копипастом
-http.get('http://map.aviasales.ru/supported_directions.json?origin_iata=LED&one_way=true&locale=ru', (res) => {
+http.get('http://min-prices.aviasales.ru/calendar_preload?origin=MOW&destination_iata=NCE&depart_date=2019-09-15&one_way=true', (res) => {
     const { statusCode } = res;
     const contentType = res.headers['content-type'];
 
@@ -27,12 +47,9 @@ http.get('http://map.aviasales.ru/supported_directions.json?origin_iata=LED&one_
     res.on('data', (chunk) => { rawData += chunk; });
     res.on('end', () => {
         try {
-            const parsedData = JSON.parse(rawData);
-            console.log(parsedData);
+            getApi(rawData);
         } catch (e) {
-            console.error(e.message);
+            console.log(e.message);
         }
     });
-}).on('error', (e) => {
-    console.error(`Got error: ${e.message}`);
 });
